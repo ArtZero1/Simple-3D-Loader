3e4067e6339552c88ef3ed47aef5e917eb0f3cb4
ArtZero <34686844+ArtZero1@users.noreply.github.com>
2021-12-30 10:26:21 +0700

    Add files via upload

---
+++ MainForm.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenPLYFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenPCDFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+        Me.MenuStrip1.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(9, 3, 0, 3)
+        Me.MenuStrip1.Size = New System.Drawing.Size(387, 31)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenPLYFileToolStripMenuItem, Me.OpenPCDFileToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(49, 25)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenPLYFileToolStripMenuItem
+        '
+        Me.OpenPLYFileToolStripMenuItem.BackColor = System.Drawing.Color.Transparent
+        Me.OpenPLYFileToolStripMenuItem.Name = "OpenPLYFileToolStripMenuItem"
+        Me.OpenPLYFileToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.OpenPLYFileToolStripMenuItem.Text = "Open PLY File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.BackColor = System.Drawing.Color.Transparent
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'OpenPCDFileToolStripMenuItem
+        '
+        Me.OpenPCDFileToolStripMenuItem.Name = "OpenPCDFileToolStripMenuItem"
+        Me.OpenPCDFileToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.OpenPCDFileToolStripMenuItem.Text = "Open PCD File"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(387, 277)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Simple 3D Loader"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenPLYFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenPCDFileToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,129 @@
+﻿
+Imports PclSharp
+Imports PclSharp.IO
+Imports PclSharp.Vis
+Imports PclSharp.Common
+
+
+Public Class MainForm
+
+    Private Shared FilePath As String = ""
+
+    Private Sub OpenPLYFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenPLYFileToolStripMenuItem.Click
+        Using ofd As New OpenFileDialog
+            ofd.Title = " Select PLY File"
+            ofd.Filter = "PLY (*.ply)|*.ply"
+            ofd.Multiselect = False
+            If ofd.ShowDialog = DialogResult.OK Then
+                FilePath = ""
+                FilePath = ofd.FileName
+                PLYLoader(FilePath)
+            End If
+        End Using
+    End Sub
+
+    Private Sub OpenPCDFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenPCDFileToolStripMenuItem.Click
+        Using ofd As New OpenFileDialog
+            ofd.Title = " Select PCD File"
+            ofd.Filter = "PLY (*.pcd)|*.pcd"
+            ofd.Multiselect = False
+            If ofd.ShowDialog = DialogResult.OK Then
+                FilePath = ""
+                FilePath = ofd.FileName
+                PCDLoader(FilePath)
+            End If
+        End Using
+    End Sub
+
+    Private Shared Sub PLYLoader(ByVal file As String)
+        Try
+            Using cloud As New PointCloudOfXYZ()
+                Using reader As New PLYReader
+                    reader.Read(file, cloud)
+                End Using
+                TransformTest(cloud)
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Shared Sub PCDLoader(ByVal file As String)
+        Try
+            Using cloud As New PointCloudOfXYZ()
+                Using reader As New PCDReader
+                    reader.Read(file, cloud)
+                End Using
+                TransformTest(cloud)
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Shared Sub TransformTest(ByVal cloud As PointCloudOfXYZ)
+        showOnce(cloud)
+        Dim cloudTransformed As New PointCloudOfXYZ()
+
+        Dim mtx As New Eigen.Matrix4f()
+        mtx(0, 0) = 0.0F
+        mtx(0, 1) = -1.0F
+        mtx(0, 2) = 0.0F
+        mtx(0, 3) = -80.0F
+
+        mtx(1, 0) = 0.0F
+        mtx(1, 1) = 0.0F
+        mtx(1, 2) = -1.0F
+        mtx(1, 3) = -200.0F
+
+        mtx(2, 0) = 1.0F
+        mtx(2, 1) = 0.0F
+        mtx(2, 2) = 0.0F
+        mtx(2, 3) = 4000.0F
+
+        mtx(3, 0) = 0.0F
+        mtx(3, 1) = 0.0F
+        mtx(3, 2) = 0.0F
+        mtx(3, 3) = 1.0F
+
+        Transforms.TransformPointCloud(cloud, cloudTransformed, mtx)
+        Show3D(cloudTransformed)
+    End Sub
+
+    Private Shared Sub showOnce(ByVal cloud As PointCloudOfXYZ)
+        Using visualizer As New Visualizer("a window")
+            visualizer.AddPointCloud(cloud)
+            visualizer.SetPointCloudRenderingProperties(RenderingProperties.PointSize, 2)
+            visualizer.SetPointCloudRenderingProperties(RenderingProperties.Opacity, 1)
+            visualizer.SetBackgroundColor(0, 0, 55)
+            visualizer.AddCoordinateSystem()
+            visualizer.AddText3D("test001", New PclSharp.Struct.PointXYZ(), 1, 1, 1, 1, "2")
+            Dim po As New PclSharp.Struct.PointXYZ()
+            po.X = 0
+            po.Y = 0
+            po.Z = 0
+            ' visualizer.AddSphere(po, 5);
+            visualizer.AddCube(0, 2, 0, 3, 0, 5)
+            visualizer.SetCameraPosition(0, 0, 0, 0, 0, 1)
+            visualizer.SpinOnce(100)
+        End Using
+    End Sub
+
+    Private Shared Sub Show3D(ByVal cloud As PointCloudOfXYZ)
+        Using viewer As Visualizer = New Visualizer("3D Viewer")
+            viewer.AddPointCloud(cloud)
+            viewer.SetPointCloudRenderingProperties(RenderingProperties.PointSize, 2)
+            viewer.SetPointCloudRenderingProperties(RenderingProperties.Opacity, 1)
+            viewer.SetBackgroundColor(0, 0, 55)
+            Do While Not viewer.WasStopped
+                viewer.SpinOnce(100)
+            Loop
+        End Using
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Close()
+    End Sub
+
+
+End Class
